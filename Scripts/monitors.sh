3de54c07f4460834602c54be81a4d28f2e0f084a
garrett503 <ZborilPatriot125412@hotmail.com>
2021-01-29 23:51:51 -0800

    edits

--- Scripts/monitors.sh
+++ Scripts/monitors.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-#left monitor vertical - set primary 144hz
-xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate right --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 1440x575 --rotate normal
+#Horizontal layout
+xrandr --output	HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 2560x222 --rotate normal
+#left monitor vertical
+#xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate right --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 1440x575 --rotate normal
