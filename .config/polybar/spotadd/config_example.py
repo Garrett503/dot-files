8c68a249d97fe4b6ae13ae3af96520c98f1d025c
garrett503 <ZborilPatriot125412@hotmail.com>
2021-01-30 01:13:37 -0800

    example config

---
+++ .config/polybar/spotadd/config_example.py
@@ -0,0 +1,6 @@
+CLIENT_ID="60396783d58141c1be54f248669b9" #Get from spotify dev website
+CLIENT_SECRET="ed23aea563f9455bacebd4ef1aaa" #Get from spotify dev website
+USERNAME="username" #Your spotify username
+SCOPE="user-library-modify user-library-read,user-read-currently-playing,user-read-playback-position,user-read-playback-state,user-modify-playback-state,app-remote-control,streaming,playlist-read-private,playlist-modify-private,playlist-modify-public" #Use "playlist-modify-public" for public playlists and "playlist-modify-private" for private ones
+REDIRECT_URI="http://localhost:8888/callback" #This is what I use but not the only option
+#PLAYLIST_ID="5rvQibveyj1RVK5Jq6bp8x" #Get from playlist options > Share > Copy Spotify URI & remove "spotify:playlist:"
