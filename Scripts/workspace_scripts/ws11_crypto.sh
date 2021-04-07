f66b5001431db0053e4af9a92679adf5902f4ca1
Garrett <ZborilPatriot125412@hotmail.com>
2021-04-07 00:58:41 -0700

    gsimplecal polybar

--- Scripts/workspace_scripts/ws11_crypto.sh
+++ Scripts/workspace_scripts/ws11_crypto.sh
@@ -4,11 +4,11 @@
 #--app=http://facebook.com
 
 url1=https://www.binance.us/en/trade/pro/BTC_USD
-#url1=https://www.binance.com/en/trade/BTC_USDT?layout=pro
+#url2=https://www.binance.com/en/trade/BTC_USDT?layout=pro
 i3-msg "workspace 11:  browser news; append_layout ~/.config/i3/ws_layouts/ws11_crypto_layout.json"
-google-chrome-unstable  --class=one --no-default-browser-check $url1  &
-google-chrome-unstable  --class=two --no-default-browser-check $url1  &
-google-chrome-unstable  --class=three --no-default-browser-check $url1  &
-google-chrome-unstable  --class=four --no-default-browser-check $url1  &
-google-chrome-unstable  --class=five --no-default-browser-check $url1  &
-google-chrome-unstable  --class=six --no-default-browser-check $url1  &
+google-chrome-unstable  --class=one --no-default-browser-check $url1 &
+google-chrome-unstable  --class=two --no-default-browser-check $url1 &
+google-chrome-unstable  --class=three --no-default-browser-check $url1 &
+google-chrome-unstable  --class=four --no-default-browser-check $url1 &
+google-chrome-unstable  --class=five --no-default-browser-check $url1 &
+google-chrome-unstable  --class=six --no-default-browser-check $url1 &
