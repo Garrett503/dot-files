846487ef5224733d7be782be378b49c28e568e2e
Garrett503 <ZborilPatriot125412@hotmail.com>
2022-01-16 01:35:31 -0800

    pywal updated for discord & spotify

--- Scripts/theme_scripts/changetheme.sh
+++ Scripts/theme_scripts/changetheme.sh
@@ -8,11 +8,11 @@ cp $HOME/.cache/wal/cava.config $HOME/.config/cava/config;
 pkill -USR2 cava;
 
 #update xresources
-cp $HOME/.cache/wal/colors.Xresources $HOME/.Xresources;
-xrdb -merge ~/.Xresources;
+#cp $HOME/.cache/wal/colors.Xresources $HOME/.Xresources;
+#xrdb -merge $HOME/.Xresources;
 
 #live update spotify colors
-#spicetify watch --live-update &
+spicetify watch --live-update &
 
 #update discord theme
 $HOME/Scripts/theme_scripts/pywal-discord.sh -t default
@@ -23,26 +23,3 @@ $HOME/Scripts/theme_scripts/dunst_pywal.sh;
 #theme changed notification
 sleep 1
 $HOME/Scripts/theme_scripts/theme_change_success.sh;
-
-############################################################
-#IGNORE
-###########################################################
-#sleep 2
-#~/Scripts/theme_scripts/dunst_pywal.sh;
-#sleep 2
-#~/Scripts/theme_scripts/theme_change_success.sh; 
-#update spotify
-#killall spotify
-#sleep 1
-#spicetify --update;
-#spicetify watch --live-update
-#~/Scripts/workspace_scripts/ws4_spotify_layout.sh;
-#sleep 5
-#spicetify apply;
-#killall spotify;
-#killall urxvt;
-#sleep 1
-#~/Scripts/workspace_scripts/ws4_spotify_layout.sh;
-#update discord
-#~/.config/BetterDiscord/pydis/pywal-discord/pywal-discord abou;
-#update dunst
